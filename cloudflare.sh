54259cf6aaaa5a6d07c76d2d38134581bc0684c9
explicitworkload <52900463+explicitworkload@users.noreply.github.com>
2020-06-24 13:51:19 +0800

    Update cloudflare.sh

--- cloudflare.sh
+++ cloudflare.sh
@@ -5,8 +5,8 @@
 #
 
 ip=$(curl -s http://whatismyip.akamai.com/)
-curl -X PUT "https://api.cloudflare.com/client/v4/zones/0eeadfb9113559d8a861859c466e2e53/dns_records/9e1a7887c0fbbf68e407400f2b12a887" \
-     -H "Authorization: Bearer ZZwV34uJnn2YQP_uQ3KO9BcTZsQ-2ICrTmlxaUAX" \
+curl -X PUT "https://api.cloudflare.com/client/v4/zones/XXXXXXXXXXZONEIDXXXXXXXX/dns_records/XXXXXXXXXXXRECORD_IDXXXXXXXXXXX" \
+     -H "Authorization: Bearer XXXXXXXXAPI_TOKENXXXXXXXXXXXXX" \
      -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"vulture.testaccelerator.com","content":"'${ip}'","ttl":1,"proxied":false}'
+     --data '{"type":"A","name":"XXXXXXXSUBDOMAINXXXXXXXXXX","content":"'${ip}'","ttl":1,"proxied":false}'
 echo $ip
